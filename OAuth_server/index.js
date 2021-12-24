2d1a634f5314e81bf0ad34813f26a8bc94c08468
Lander San Millan <landersanmillan@opendeusto.es>
2021-12-24 01:08:14 +0100

    OAuth server implemented login and gateway

--- OAuth_server/index.js
+++ OAuth_server/index.js
@@ -1,24 +1,92 @@
-'use strict';
-
+'use strict'
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+var { initialize } = require("express-openapi");
+var swaggerUi = require("swagger-ui-express");
 var path = require('path');
 var http = require('http');
-
 var oas3Tools = require('oas3-tools');
-var serverPort = 9090;
+const YAML = require('yamljs');
+var alert = require('alert');
+var keys = require('../keys.json');
 
-// swaggerRouter configuration
-var options = {
-    routing: {
-        controllers: path.join(__dirname, './controllers')
-    },
-};
+const SERVER_PORT = 9090;
+var app = express();
 
-var expressAppConfig = oas3Tools.expressAppConfig(path.join(__dirname, 'api/openapi.yaml'), options);
-var app = expressAppConfig.getApp();
+const swaggerDoc = YAML.load('./api/openapi.yaml');
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDoc))
+
+app.use(express.static(__dirname + '/static'));
+
+const session = require('express-session');
+
+app.set('view engine', 'ejs');
+
+app.use(session({
+    resave: false,
+    saveUninitialized: true,
+    secret: 'SECRET' 
+}));
+
+app.get('/', function(req, res) {
+    res.render('pages/auth');
+});
+
+
+/*  PASSPORT SETUP  */
+const passport = require('passport');
+var userProfile;
+var token;
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.get('/success', (req, res) => res.send(userProfile));
+app.get('/error', (req, res) => res.send("error logging in"));
+
+passport.serializeUser(function(user, cb) {
+  cb(null, user);
+});
+
+passport.deserializeUser(function(obj, cb) {
+  cb(null, obj);
+});
+
+
+/*  GOOGLE AUTH  */
+const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+/* ############################################################################################################*/
+const GOOGLE_CLIENT_ID = keys['GOOGLE_CLIENT_ID'];
+const GOOGLE_CLIENT_SECRET = keys['GOOGLE_CLIENT_SECRET'];
+/* ############################################################################################################*/
+passport.use(new GoogleStrategy({
+    clientID: GOOGLE_CLIENT_ID,
+    clientSecret: GOOGLE_CLIENT_SECRET,
+    callbackURL: "http://127.0.0.1:9090/auth/google/callback"
+  },
+  function(accessToken, refreshToken, profile, done) {
+      userProfile = profile;
+      token = accessToken;
+      return done(null, userProfile, token);
+  }
+));
+ 
+app.get('/auth/google', 
+  passport.authenticate('google', { scope : ['profile', 'email'] }));
+ 
+app.get('/auth/google/callback', 
+  passport.authenticate('google', { failureRedirect: '/error' }),
+  function(req, res) {
+    // Successful authentication, redirect success.
+    res.redirect('http://localhost:8080/login?token=' + token );
+});
+
 
 // Initialize the Swagger middleware
-http.createServer(app).listen(serverPort, function () {
-    console.log('Your server is listening on port %d (http://localhost:%d)', serverPort, serverPort);
-    console.log('Swagger-ui is available on http://localhost:%d/docs', serverPort);
+http.createServer(app).listen(SERVER_PORT, function () {
+    console.log('Your server is listening on port %d (http://localhost:%d)', SERVER_PORT, SERVER_PORT);
+    console.log('Swagger-ui is available on http://localhost:%d/api-docs', SERVER_PORT);
 });
 
--- gbfs_visualizer_server/openapi_server/__main__.py
+++ gbfs_visualizer_server/openapi_server/__main__.py
@@ -2,12 +2,22 @@
 
 import connexion
 
+from google.oauth2 import id_token
+from google.auth.transport import requests
+
 from openapi_server import encoder
 
-from flask import Flask, url_for, redirect, render_template
+from flask import Flask, url_for, redirect, render_template, request
 from flask_babel import Babel, gettext, ngettext
 import os
 import jinja2
+
+import requests
+
+import json
+
+with open('../keys.json') as f:
+  keys = json.load(f)
    
 def main():
     openapi_path = './openapi/'
@@ -19,14 +29,27 @@ def main():
     
     #babel = Babel(app)
 
-    @app.route('/')
-    def play_game():
-        return redirect(url_for('map'))
+    @app.route('/login')
+    def maint():
+        token = request.args.get('token', default = 'noToken', type = str)
+        return redirect(url_for('map', token= str(token)))
 
     @app.route('/map')
     def map():
-        print("CALL TO /MAP")
-        return render_template('map.html')
+        token = request.args.get('token', default = 'noToken', type = str)
+        # Specify the CLIENT_ID of the app that accesses the backend:
+        response = requests.get('https://www.googleapis.com/oauth2/v2/tokeninfo?access_token=' + token)
+        
+        if(response.status_code == 200):
+            # ID token is valid. Get the user's Google Account ID from the decoded token.
+            print("CALL TO /MAP")
+            return render_template('map.html', MAPS_API_KEY=keys["MAPS_API_KEY"])
+        else: 
+            # Invalid token
+            print("BACK TO LOGIN")
+            return redirect('http://127.0.0.1:9090/')
+
+
 
     app.run(port=8080)
 
